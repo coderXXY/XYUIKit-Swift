af4060e80a78d02f209944956f143a9711e8b459
coderXY <coderxxy@163.com>
2023-12-23 20:44:53 +0800

    【title】添加公用宏以及方法
    【description】
    
    【developer】coderxxy

---
+++ XYUIKit-Swift/Classes/XYMacroDefine.swift
@@ -0,0 +1,13 @@
+//
+//  XYMacroDefine.swift
+//  XYUIKit-Swift
+//
+//  Created by coderXY on 2023/12/23.
+//
+
+import UIKit
+/** XYSCREEN_W */
+let XYSCREEN_W = UIScreen.main.bounds.size.width
+/** XYSCREEN_H */
+let XYSCREEN_H = UIScreen.main.bounds.size.height
+
