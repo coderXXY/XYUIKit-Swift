a49156385a58a0f203e379170fb48c4acc673ccc
coderXY <coderxxy@163.com>
2023-12-24 10:41:14 +0800

    【title】修改debug log 方法 XYDebugPrint
    【description】
    
    【developer】coderxxy

--- XYUIKit-Swift/Classes/XYCommonService.swift
+++ XYUIKit-Swift/Classes/XYCommonService.swift
@@ -9,7 +9,7 @@ import UIKit
 /** 通用方法 */
 public class XYCommonService: NSObject {
     /** debug log */
-    func XYDebugPrint(_ items: String..., filename: String = #file, function : String = #function, line: Int = #line, separator: String = " ", terminator: String = "\n") {
+    static func XYDLog(_ items: String..., filename: String = #file, function : String = #function, line: Int = #line, separator: String = " ", terminator: String = "\n") {
 #if DEBUG
         let pretty = "===### \(URL(fileURLWithPath: filename).lastPathComponent) [#\(line)] \(function) ###=== \n"
         let output = items.map { "\($0)" }.joined(separator: separator)
